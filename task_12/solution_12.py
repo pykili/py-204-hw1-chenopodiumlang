eeb73eb678cc0f5beee7ba3dfb06ccc95f86e4f8
chenopodiumlang <72070118+chenopodiumlang@users.noreply.github.com>
2020-10-23 23:37:13 +0300

    solved 12

--- task_12/solution_12.py
+++ task_12/solution_12.py
@@ -1,1 +1,11 @@
-# your code here
+my_string = input()
+length = int(len(my_string))
+twos = ""
+occured_twice = False
+for i in range (length):
+	if i != length-1:
+		pair = my_string[i] + my_string[i+1]
+		if pair in str(twos):
+			occured_twice = True
+		twos = twos + pair
+print(occured_twice)
