cc9fed72ce3b32dd124f9a03b865545d110909f5
chenopodiumlang <72070118+chenopodiumlang@users.noreply.github.com>
2020-10-23 19:55:49 +0300

    Update solution_5.py
    мне питон пишет, что в строке 28 IndexError: string index out of range. Что это значит, я понимаю, но не понимаю, что конкретно я делаю не так. Помогите, пожалуйста!

--- task_5/solution_5.py
+++ task_5/solution_5.py
@@ -1,1 +1,40 @@
-# your code here
+front_vow = "eiöü"
+back_vow = "aıou"
+vows = "eiöüaıou"
+user_input = input()
+a = int(len(user_input))
+i = 0
+for i in range (a):
+	if user_input[i] == '\t':
+		t3 = i+1
+for i in range (t3-1):
+	if user_input[i] == '\t':
+		t2 = i+1
+for i in range (t2-1):
+	if user_input[i] == '\t':
+		t1 = i+1
+form = user_input[t1]
+lemma = user_input[t2]
+for i in range (t1+1, t2-1):
+	form = form + user_input[i]
+for i in range (t2+1, t3-1):
+	lemma = lemma + user_input[i]
+l = int(len(lemma))
+f = int(len(form))
+correct = 0
+wrong = 0
+if form != lemma and form[0] == lemma[0] and lemma in str(form):
+	for k in range (t2+2, t3-1):
+		if lemma[k] in vows:
+			v = lemma[k]
+	for k in range (t1+2, t2-1):
+		if form[k] in vows:
+			if form[k] in front_vow and v in front_vow:
+				correct = correct + 1
+			elif form[k] in back_vow and v in back_vow:
+				correct = correct + 1
+			else:
+				wrong = wrong + 1
+print("correct: ", correct)
+print("wrong: ", wrong)
+
