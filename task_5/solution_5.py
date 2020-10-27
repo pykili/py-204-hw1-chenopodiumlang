2cd2bf94ff5341bbe1c2beff8f221a9a3791607e
chenopodiumlang <72070118+chenopodiumlang@users.noreply.github.com>
2020-10-28 00:02:31 +0300

    update 5
    оно не до конца работает. будет доделываться

--- task_5/solution_5.py
+++ task_5/solution_5.py
@@ -23,18 +23,22 @@ l = int(len(lemma))
 f = int(len(form))
 correct = 0
 wrong = 0
-if form != lemma and form[0] == lemma[0] and lemma in str(form):
-	for k in range (t2+2, t3-1):
-		if lemma[k] in vows:
-			v = lemma[k]
-	for k in range (t1+2, t2-1):
-		if form[k] in vows:
-			if form[k] in front_vow and v in front_vow:
-				correct = correct + 1
-			elif form[k] in back_vow and v in back_vow:
-				correct = correct + 1
-			else:
-				wrong = wrong + 1
+if form != lemma and lemma in str(form):
+	for i in range (l):
+		if form[i] == lemma[i]:
+			for k in range (t2+2, t3+1):
+				if lemma[k+1] in vows:
+					v = lemma[k]
+			for k in range (t1+2, t2-1):
+				if form[k] in vows:
+					if form[k] in front_vow and v in front_vow:
+						correct = correct + 1
+						print(correct)
+					elif form[k] in back_vow and v in back_vow:
+						correct = correct + 1
+						print(correct)
+					else:
+						wrong = wrong + 1
+						print(wrong)
 print("correct: ", correct)
 print("wrong: ", wrong)
-
