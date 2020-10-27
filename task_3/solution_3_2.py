81a04881d346e9ad765031c526d2eb5e549deb5e
chenopodiumlang <72070118+chenopodiumlang@users.noreply.github.com>
2020-10-27 23:11:25 +0300

    update 3.2

--- task_3/solution_3_2.py
+++ task_3/solution_3_2.py
@@ -3,7 +3,6 @@ for k in range (10):
 	user_input = input()
 	length = int(len(user_input))
 	for i in range (length):
-		if user_input[i] in str(alphabet):
-			alphabet = alphabet
-		else: alphabet = alphabet + user_input[i]
+		if user_input[i] not in str(alphabet):
+			alphabet = alphabet + user_input[i]
 print(alphabet)
